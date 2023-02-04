8e852d7422dba53bb442ca2503e18d7165580304
Coteh <3276350+Coteh@users.noreply.github.com>
2023-02-04 12:02:12 -0500

    Redesign log view to be a table instead [WIP]

--- src/component/LogView.tsx
+++ src/component/LogView.tsx
@@ -1,8 +1,20 @@
-import { Box, Flex } from '@chakra-ui/react';
+import {
+    Box,
+    Flex,
+    Link,
+    Table,
+    TableContainer,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+} from '@chakra-ui/react';
 import { QueryResults } from '../action/query';
 import { QueryDefinition } from '../context/QueryControlContext';
-import { useState } from 'react';
-import { Virtuoso } from 'react-virtuoso';
+import { useState, useRef, forwardRef, Fragment } from 'react';
+import { TableVirtuoso, TableVirtuosoHandle } from 'react-virtuoso';
+import { RiArrowRightSFill, RiArrowDownSFill } from 'react-icons/ri';
 
 type LogViewProps = {
     queryDefinition: QueryDefinition;
@@ -20,6 +32,7 @@ export const LogView: React.FC<LogViewProps> = ({
     const [expandedRows, setExpandedRows] = useState<Map<number, boolean>>(
         new Map()
     );
+    const ref = useRef<TableVirtuosoHandle>(null);
 
     const expandCollapseRow = (index: number, expanded: boolean) => {
         setExpandedRows((expandedRows) => {
@@ -29,90 +42,231 @@ export const LogView: React.FC<LogViewProps> = ({
         });
     };
 
+    const HeaderRow = () => {
+        if (!queryResults || !queryResults.length) {
+            return <></>;
+        }
+        const queryResult = queryResults[0];
+
+        return (
+            <Tr>
+                {queryResult.map((column) => (
+                    <Th
+                        width={200}
+                        style={{
+                            background: 'blue',
+                            color: 'white',
+                        }}
+                    >
+                        {(!queryDefinition.filteredColumns ||
+                            queryDefinition.filteredColumns.length === 0 ||
+                            queryDefinition.filteredColumns.includes(
+                                column.Field
+                            )) &&
+                            column.Field}
+                    </Th>
+                ))}
+            </Tr>
+        );
+    };
+
     const Row = (index: number) => {
         const expanded = expandedRows.get(index);
-        if (!queryResults) {
+        if (!queryResults || !queryResults.length) {
             return <></>;
         }
         const queryResult = queryResults[index];
 
+        let ci = 0;
+
         return (
-            <Flex border="1px solid black" borderLeft="none" borderRight="none">
-                <Box width="200px" flexShrink={0}>
-                    {queryResult[0].Value}
-                </Box>
-                <Box width="50px" flexShrink={0}></Box>
-                <Flex flexDirection={'column'} flex={1} width={500}>
-                    <Box>
-                        {queryResult.map(
-                            (column) =>
-                                (!queryDefinition.filteredColumns ||
-                                    queryDefinition.filteredColumns.length ===
-                                        0 ||
-                                    queryDefinition.filteredColumns.includes(
-                                        column.Field
-                                    )) && (
-                                    <>
-                                        <span>
-                                            {column.Field}={column.Value}
-                                        </span>
-                                        <br />
-                                    </>
-                                )
-                        )}
-                    </Box>
-                    <a
-                        onClick={() => expandCollapseRow(index, !expanded)}
-                        href="#"
-                    >
-                        {expanded ? 'collapse' : 'expand'}
-                    </a>
-                    {expanded && (
-                        <Box>
-                            {queryResult.map((column) => (
-                                <>
-                                    <span>
-                                        {column.Field}={column.Value}
-                                    </span>
-                                    {queryDefinition.filteredColumns &&
-                                    queryDefinition.filteredColumns.includes(
-                                        column.Field
-                                    ) ? (
-                                        <a
-                                            style={{
-                                                marginLeft: '8px',
-                                            }}
-                                            href="#"
-                                            onClick={() =>
-                                                onColumnRemove(column.Field)
-                                            }
-                                        >
-                                            remove
-                                        </a>
-                                    ) : (
-                                        <a
-                                            style={{
-                                                marginLeft: '8px',
-                                            }}
-                                            href="#"
-                                            onClick={() =>
-                                                onColumnAdd(column.Field)
-                                            }
-                                        >
-                                            add
-                                        </a>
-                                    )}
-                                    <br />
-                                </>
-                            ))}
-                        </Box>
-                    )}
-                </Flex>
-            </Flex>
+            <Fragment key={`Row_${index}`}>
+                {queryResult.map((column) => {
+                    // TODO fix filtered columns not taking up full width of table
+                    if (
+                        queryDefinition.filteredColumns &&
+                        queryDefinition.filteredColumns.length > 0 &&
+                        !queryDefinition.filteredColumns.includes(column.Field)
+                    ) {
+                        return <></>;
+                    }
+                    ci += 1;
+                    return (
+                        <Td
+                            className="result-td"
+                            flexDirection={'column'}
+                            width={500}
+                            maxWidth={1000}
+                            // TODO if just selecting text, do not expand/collapse the row
+                            onClick={() => {
+                                expandCollapseRow(index, !expanded);
+                                // TODO fix table scrolling back to the top
+                                setTimeout(() => {
+                                    if (!ref.current) {
+                                        return;
+                                    }
+                                    ref.current.scrollIntoView({
+                                        index: index + 10,
+                                        behavior: 'auto',
+                                        done: () => {},
+                                    });
+                                }, 0);
+                            }}
+                        >
+                            <Flex justifyContent={'space-between'}>
+                                {ci === 1 ? (
+                                    <Box>
+                                        {expanded ? (
+                                            <RiArrowDownSFill />
+                                        ) : (
+                                            <RiArrowRightSFill />
+                                        )}
+                                    </Box>
+                                ) : (
+                                    <></>
+                                )}
+                                <Box whiteSpace={'normal'}>{column.Value}</Box>
+                            </Flex>
+                        </Td>
+                    );
+                })}
+                {/* TODO fix the table horizontal scroll not appearing unless you scroll the document, the heights of the dashboard need to be adjusted so that the whole thing will fit in the view without scrolling */}
+            </Fragment>
         );
     };
 
     return (
-        <Virtuoso totalCount={queryResults?.length || 0} itemContent={Row} />
+        <TableVirtuoso
+            ref={ref}
+            data={queryResults}
+            components={{
+                Scroller: forwardRef((props, ref) => (
+                    <TableContainer
+                        {...props}
+                        ref={ref}
+                        overflowY={'initial'}
+                        overflowX={'initial'}
+                    />
+                )),
+                Table: (props) => <Table {...props} variant="striped" />,
+                TableHead: Thead,
+                TableRow: (props) => {
+                    const index = props['data-index'];
+                    const expanded = expandedRows.get(index);
+                    const queryResult = queryResults
+                        ? queryResults[index]
+                        : undefined;
+                    return (
+                        <>
+                            <Tr {...props} />
+                            {queryResult && expanded && (
+                                <Tr>
+                                    <Td colSpan={5}>
+                                        <Box>
+                                            {queryResult.map((column) => (
+                                                <>
+                                                    <span>
+                                                        {column.Field}=
+                                                        {column.Value}
+                                                    </span>
+                                                    {queryDefinition.filteredColumns &&
+                                                    queryDefinition.filteredColumns.includes(
+                                                        column.Field
+                                                    ) ? (
+                                                        <Link
+                                                            style={{
+                                                                marginLeft:
+                                                                    '8px',
+                                                            }}
+                                                            href="#"
+                                                            onClick={() => {
+                                                                onColumnRemove(
+                                                                    column.Field
+                                                                );
+                                                                // TODO fix table scrolling back to the top
+                                                                setTimeout(
+                                                                    () => {
+                                                                        if (
+                                                                            !ref.current
+                                                                        ) {
+                                                                            return;
+                                                                        }
+                                                                        ref.current.scrollIntoView(
+                                                                            {
+                                                                                index:
+                                                                                    index +
+                                                                                    10,
+                                                                                behavior:
+                                                                                    'auto',
+                                                                                done: () => {},
+                                                                            }
+                                                                        );
+                                                                    },
+                                                                    0
+                                                                );
+                                                            }}
+                                                        >
+                                                            remove
+                                                        </Link>
+                                                    ) : (
+                                                        <Link
+                                                            style={{
+                                                                marginLeft:
+                                                                    '8px',
+                                                            }}
+                                                            href="#"
+                                                            onClick={() => {
+                                                                onColumnAdd(
+                                                                    column.Field
+                                                                );
+                                                                // TODO fix table scrolling back to the top
+                                                                setTimeout(
+                                                                    () => {
+                                                                        if (
+                                                                            !ref.current
+                                                                        ) {
+                                                                            return;
+                                                                        }
+                                                                        ref.current.scrollIntoView(
+                                                                            {
+                                                                                index:
+                                                                                    index +
+                                                                                    10,
+                                                                                behavior:
+                                                                                    'auto',
+                                                                                done: () => {},
+                                                                            }
+                                                                        );
+                                                                    },
+                                                                    0
+                                                                );
+                                                            }}
+                                                        >
+                                                            add
+                                                        </Link>
+                                                    )}
+                                                    <br />
+                                                </>
+                                            ))}
+                                        </Box>
+                                    </Td>
+                                </Tr>
+                            )}
+                        </>
+                    );
+                },
+                TableBody: forwardRef((props, ref) => (
+                    <Tbody
+                        {...props}
+                        ref={ref}
+                        style={{
+                            overflow: 'auto',
+                        }}
+                    />
+                )),
+            }}
+            fixedHeaderContent={HeaderRow}
+            itemContent={Row}
+        />
     );
 };
